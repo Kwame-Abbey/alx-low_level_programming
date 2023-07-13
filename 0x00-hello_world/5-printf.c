b9cd79892bf5b692cfac92b6ac1412bac3fb67a1
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-13 12:34:34 -0700

    Use printf to ouput a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - (Outputs a string using the printf function)
+ *
+ * Retrun: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
