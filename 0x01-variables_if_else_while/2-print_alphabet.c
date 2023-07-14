c7858b7562896ed01652eff67bc5c9980668a1bc
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-14 05:25:48 -0700

    Print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - (A function that outputs the alphabets in lowercase)
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	return (0);
+}
