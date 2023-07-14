106187e6e85a8fad29c47d3b086846b11c495aa9
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-14 06:21:45 -0700

    Single digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - (A function that outputs single digit numbers)
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
