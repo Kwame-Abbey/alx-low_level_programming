2877a1900d2f90f5b5058dfb45028307399d7c61
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-17 13:53:23 -0700

    Hmm after several days of premium tears

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - (Prints out all numbers between 00 and 99)
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+
+			putchar(i);
+			putchar(j);
+
+			if (i != 56 && j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			} else
+			{
+				break;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
