99f76b937cc131abacf1b2a1e529b40defd3d26a
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-18 14:03:20 -0700

    Print alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - (Prints the alphabets in lowercase)
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar('a');
+	}
+	putchar('\n');
+	return (0);
+}
