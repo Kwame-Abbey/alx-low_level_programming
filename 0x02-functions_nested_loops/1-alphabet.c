e4ccd873190776cdfe7a6156368e30ebba1e0d87
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-18 14:11:58 -0700

    Update task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,10 +10,15 @@ int main(void)
 {
 	char a = 'a';
 
-	while (a <= 'z')
-	{
-		_putchar('a');
-	}
+	print_alphabet();
 	_putchar('\n');
 	return (0);
+}
+
+void print_alphabet(void)
+{
+	while (a <= 'z')
+	{
+		_putchar(a);
+	}
 }
