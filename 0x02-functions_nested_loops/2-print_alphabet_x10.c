04285239b80ccd3dae14be80210144d5577f23e5
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-18 14:37:33 -0700

    Update task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,14 @@
 void print_alphabet_x10(void)
 {
 	int n;
+	char a;
 
 	for (n = 0; n < 10; n++)
 	{
-		for (char a = 'a'; a <= 'z'; a++)
+		for (a = 'a'; a <= 'z'; a++)
 		{
 			_putchar(a);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
