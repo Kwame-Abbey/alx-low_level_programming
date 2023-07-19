9b475ada96edc6abcddd94ecd5db601f8ac27c37
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-19 10:16:50 -0700

    Check alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - (Checks for an alphabetic character).
+ *
+ * @c: The character to return.
+ *
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
