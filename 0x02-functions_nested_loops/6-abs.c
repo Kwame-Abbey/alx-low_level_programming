c99e633ffc7799f82071ddfabb4ec6216cc865ab
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-19 11:03:28 -0700

    Complete task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - (Computes the absolute value of an integer).
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		_putchar(n);
+	} else if (n < 0)
+	{
+		_putchar(n + (n * 2);
+	}
+
+	return (0);
+}
