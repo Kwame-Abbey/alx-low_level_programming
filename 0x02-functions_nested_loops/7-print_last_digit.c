fe631721693750782fd8c687ae31fb25602c93e4
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-19 11:23:23 -0700

    Complete task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - (Prints last digit ofa number).
+ *
+ * @n: Character to be returned.
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
