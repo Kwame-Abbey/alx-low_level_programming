9b763cc05f1e6ffe5b2b55f6dd622f38c9ada45b
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-20 09:43:56 -0700

    Correct new line _putchar

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,10 @@ void print_most_numbers(void)
 
 	for (a = 48; a <= 57; a++)
 	{
-		if ((a != 50 && a != 52))
+		if (a != 50 && a != 52)
 		{
 			_putchar(a);
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
