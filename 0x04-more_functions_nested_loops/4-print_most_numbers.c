ecdbe388898344d5f0bf7fda1d1d12de967d9445
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-20 09:38:38 -0700

    Print 0 to 9 exc. 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - (Print 0 to 9 excluding 2 and 4).
+ *
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 && a != 52)
+		{
+			_putchar(a);
+		}
+	}
+	putchar('\n');
+}
