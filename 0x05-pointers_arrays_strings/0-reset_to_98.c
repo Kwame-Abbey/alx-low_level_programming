255d9b514d6dab77938c831163e850bbaa6ff5ae
Kwame Abbey <constancekwameabbey@gmail.com>
2023-07-24 06:01:37 -0700

    Update value of parameter using a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - (Updates the value of a parameter using a pointer)
+ * @n: Parameter updated
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Basics of pointers, arrays and strings in C.
