fbb71e50dbeac090b43b8e721a28d0e17459eb64
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-07-26 16:14:03 +0000

    Concatenate two strings

---
+++ 0x06-pointers_array_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *strcat - (Concatenates two strings)
+ * @dest: Destination of concatenated strings
+ * @src: String to be appended
+ * Return: Concatenated string
+ */
+
+char *strcat(char *dest, char *src)
+{
+	int i, len = 0;
+
+	while (*(dest + len) != '\0')
+		len++;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+		*(dest + (len + i)) = *(src + i);
+
+	*(dest + (len + i)) = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_array_strings/README.md
@@ -0,0 +1,1 @@
+More practical examples on pointers, arrays and strings
