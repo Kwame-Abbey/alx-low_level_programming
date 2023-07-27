c4d000405bdfada4f260f48d84b6e3fcd5a2a61b
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-07-27 07:49:55 +0000

    Return should be any value other than 1 or -1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, is_same = 0, sign = 1;
+	int i, is_same = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 		if (s1[i] != s2[i])
 		{
 			if (s1[i] > s2[i])
 			{
-				is_same = 1;
+				is_same = *(s1 + i) - *(s2 + i);
 				break;
 			} else if (s1[i] < s2[i])
 			{
-				is_same = 1;
-				sign *= -1;
+				is_same = *(s1 + i) - *(s2 + i);
 				break;
 			}
 		}
-	return (is_same * sign);
+	return (is_same);
 }
