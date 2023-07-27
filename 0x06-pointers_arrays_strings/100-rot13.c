35f50f03c68006318cc08930cb9b807eaee45198
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-07-27 17:11:06 +0000

    print number

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *rot13 - (Encodes a string using rot13)
+ * @s: string to be encoded
+ * Return: Encoded string
+ */
+
+char *rot13(char *s)
+{
+	int str_count, rot;
+	char a[] = {};
+}
