3533611f5e42fdc595baa4d84021f693bc3b3a7f
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-10 13:08:08 +0000

    Update task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		*(ptr + i) = *(s1 + i);
 
 	/* Appending n bytes of s2 to allocated memory */
-	for (i = 0; i < len1 + n; i++)
+	for (i = 0; i < n; i++)
 		*(ptr + len1 + i) = *(s2 + i);
 	*(ptr + len1 + n) = '\0';
 
