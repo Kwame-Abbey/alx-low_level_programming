ae208dfe0fbad921faf1e9e33aff9504226d23dc
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-10 13:20:53 +0000

    Update task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc - (Allocates memory for an array, using malloc)
@@ -19,6 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
+	memset(ptr, 0, nmemb * size);
 
 	return (ptr);
 }
