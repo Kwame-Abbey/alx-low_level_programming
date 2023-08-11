956f00a5f2c4b6ce84ebeb17de1d9e120e6dec93
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-11 08:15:08 +0000

    update task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,8 +15,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr == NULL)
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 	if (new_size > old_size)
 	{
 		p = malloc(new_size);
