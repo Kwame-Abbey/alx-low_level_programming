9a4466b7269cec66f0222ba7c84acf84e4d64f5c
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-14 12:22:40 +0000

    Updatetask 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - Dog with following element
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: This struct holds several elements of a dog
+ */
+
 struct dog
 {
 	char *name;
