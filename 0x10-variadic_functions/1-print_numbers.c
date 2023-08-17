7e020f15e16b262ffecf1f3b87a855ba52ca7720
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-17 13:42:11 +0000

    Update task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,19 +14,16 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int num;
 	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		num = va_arg(args, int);
-		if (i < n - 1)
-		{
-			printf("%d%s ", num, separator);
-		} else
-			printf("%d\n", num);
+		printf("%d", va_arg(args, int));
+		if (i < n - 1 && separator)
+			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(args);
 }
