6c05da0aeaf589a609cb3112a8da9dd76fbb8c62
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-24 16:16:31 +0000

    Print something before main function executes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * before_main_fun - Prints a statement before main executes
+ *
+ * Return: void
+ */
+
+void before_main_fun(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
