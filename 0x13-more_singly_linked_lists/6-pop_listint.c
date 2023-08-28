bcc0266ab3128d2a0b19f56ffe0a015c82481333
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-28 14:17:17 +0000

    Delete the head node of a linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ * @head: Head node of a linked list
+ * Return: Head node's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp_head = *head;
+	int node_data;
+
+	*head = (*head)->next;
+	node_data = temp_head->n;
+	free(temp_head);
+
+	return (node_data);
+}
