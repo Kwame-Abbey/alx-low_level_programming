29c71442c4fdb5b3c37d30c516932173663d2533
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-29 08:24:03 +0000

    Update task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	curr_node = *head;
 	prev_node = NULL;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
