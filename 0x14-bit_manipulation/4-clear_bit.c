7a8ac2b698f45720581667e12b158bae756d5d08
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-08-31 17:51:17 +0000

    Clear a bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0
+ * @n: Number
+ * @index: index
+ * Return: 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n &= ~(1 << index);
+
+	return (1);
+}
