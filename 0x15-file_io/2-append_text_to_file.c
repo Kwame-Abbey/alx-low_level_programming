3e1defd9324e3de2494bef8d3db2f80d56a22efb
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-09-04 14:13:04 +0000

    Update task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		fd = open(filename, O_WRONLY | O_APPEND);
 		if (fd == -1)
 			return (-1);
-		length = 0;
+		len = 0;
 		while (*(text_content + len) != '\0')
 			len++;
 		r_write = write(fd, text_content, len);
