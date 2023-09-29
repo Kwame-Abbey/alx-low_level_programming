8916bf8957e6646612e63d55179d2e88b63461b4
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-09-29 14:41:23 +0000

    Update

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	long int i, x = 1, y = 2, sum i = 0, even_Sum = 0;
+	long int i, x = 1, y = 2, sum = 0, even_sum = 0;
 
 	for (i = 0; i < 49; i++)
 	{
 		if ((y % 2 == 0) && (y <= 4000000))
 		{
-			even_Sum = even_Sum + y;
+			even_sum = even_sum + y;
 		}
 		sum = x + y;
 		x = y;
 		y = sum;
 
 	}
-	printf("%ld\n", even_Sum);
+	printf("%ld\n", even_sum);
 	return (0);
 }
