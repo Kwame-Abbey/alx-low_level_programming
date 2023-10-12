69e87b67075a26b19612ffc6dd8bb86ec1ad1352
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-12 13:28:27 +0000

    Fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,35 +10,30 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h, *next;
-	dlistint_t *new_node;
-	size_t i;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
+	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		new_node->next = *h;
-		new_node->prev = NULL;
-		(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
 
-	for (i = 0; i < (idx - 1); i++)
-	{
-		if (current->next == NULL)
-			return (NULL);
-		current = current->next;
-	}
-	next = current->next;
-	new_node->next = next;
-	new_node->prev = current;
-	next->prev = new_node;
-	current->next = new_node;
-	return (new_node);
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
