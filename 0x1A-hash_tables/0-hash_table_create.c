d321d7fd12db033b68cc199a8c0e4e13a26709a1
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-02 08:51:41 +0000

    create new hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size of hast table to be created
+ *
+ * Return: pointer to newly created hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_ht;
+	unsigned long int i;
+
+	new_ht = malloc(sizeof(hash_table_t));
+	if (new_ht == NULL)
+		return (NULL);
+
+	new_ht->size = size;
+	new_ht->array = malloc(sizeof(hash_table_t *) * size);
+	if (new_ht->array == NULL)
+	{
+		free(new_ht);
+		return (NULL);
+	}
+
+	for (i = 0; i < new_ht->size; i++)
+		new_ht->array[i] = NULL;
+	return (new_ht);
+}
