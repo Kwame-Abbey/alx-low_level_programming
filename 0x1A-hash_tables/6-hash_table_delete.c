a4fa767dcf2bbff2e1783dbbc53dcf58ef93bfd3
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-03 11:23:01 +0000

    free a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *temp;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		for (; node != NULL; node = temp)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
